44455c746ca53a4616684e6014cd1888a842238c
JOY SINHA <51963863+sinhajoy@users.noreply.github.com>
2021-07-24 23:58:35 +0530

    Create script1.sql

---
+++ IF-ELSE/script1.sql
@@ -0,0 +1,12 @@
+SET SERVEROUTPUT ON
+DECLARE 
+    v_num NUMBER := 25; 
+BEGIN 
+    IF mod(v_num,2)=0  THEN 
+        DBMS_OUTPUT.PUT_LINE(v_num || ' IS EVEN'); 
+    ELSE 
+        DBMS_OUTPUT.PUT_LINE(v_num || ' IS ODD'); 
+    END IF; 
+        DBMS_OUTPUT.PUT_LINE('Construction Complete'); 
+         
+END; 
