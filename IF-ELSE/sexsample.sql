4e813a8cf0c0f71e22f8276b49181c826cd18dd0
JOY SINHA <51963863+sinhajoy@users.noreply.github.com>
2021-07-27 01:20:48 +0530

    Update sexsample.sql

--- IF-ELSE/sexsample.sql
+++ IF-ELSE/sexsample.sql
@@ -13,3 +13,21 @@ BEGIN
         DBMS_OUTPUT.PUT_LINE('We can not Fuck the man');
     END IF;
 END;
+/
+-- SET SERVEROUTPUT ON;
+DECLARE
+    nam varchar(30);
+    age number;
+    sex varchar(7);
+    varginity varchar(7);
+BEGIN
+    nam := 'Sunny Leone';
+    age :=20;
+    sex :='Female'; -- AND / OR
+    
+    IF sex='Female' OR age<15  THEN
+        DBMS_OUTPUT.PUT_LINE('We can Fuck the young lady ' || nam);
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('We can not Fuck the man');
+    END IF;
+END;
